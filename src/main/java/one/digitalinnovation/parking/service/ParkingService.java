dcc47722ac8a7919745930444c6b79e9e53ee91a
GeoMorttis <rodrigues1975@outlook.com>
2022-08-05 18:23:01 -0300

    First endPoint List

---
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.parking.service;
+
+import one.digitalinnovation.parking.model.Parking;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ParkingService {
+
+    private static final Map<String, Parking> parkingMap = new HashMap<>();
+
+    static {
+        var id = getUUID();
+        Parking parking = new Parking(id, "DXJ-5151", "SC", "LANCIA DELTA", "BRANCO");
+        parkingMap.put(id, parking);
+    }
+
+    public List<Parking> findAll() {
+        return new ArrayList<>(parkingMap.values());
+    }
+
+    private static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+}
